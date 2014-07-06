9ce030e6cf4bd072cff581808731c864061fd83a
Jérôme FIX <jerome.fix@zapoyok.info>
2014-07-06 21:32:58 +0200

    Mise en place de l'internationalisation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Menu\CoreBundle\MenuCoreBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
